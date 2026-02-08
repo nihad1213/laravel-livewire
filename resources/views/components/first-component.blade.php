f8037a3142da9e1a11b7c5210740b3f2ead743be
nihad1213 <nihad.nemetli@gmail.com>
2026-02-08 12:47:58 +0400

    new: first component created

---
+++ resources/views/components/first-component.blade.php
@@ -0,0 +1,16 @@
+<?php
+
+use Livewire\Component;
+
+new class extends Component
+{
+    public function render()
+    {
+        return view('components.first-component');
+    }
+};
+?>
+
+<div>
+    <h1>Welcome to Laravel Livewire!</h1>
+</div>
